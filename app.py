7e3f5081a1652f3835b9119220c8be0668302ed2
m-weitzel <m_weitzel@gmx.net>
2020-08-11 17:43:26 +0200

    testing what broke

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
+# app.config.from_object(os.environ['APP_SETTINGS'])
 
 print(os.environ['APP_SETTINGS'])
 
