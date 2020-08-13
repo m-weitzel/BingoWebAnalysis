cb1c1844bbdcffe9380a876c402326c922bc3a2a
Max Wei <fenyan.mw@gmail.com>
2020-08-13 16:02:18 +0200

    added bingo analysis functionality. website now shows goal ovw

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+import pickle
+from models import return_goallist
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
-print(os.environ['APP_SETTINGS'])
 
-app.config.from_object(os.environ['APP_SETTINGS'])
-
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	goal_df_repr = return_goallist()
+	# return render_template('index.html', goals=goal_df_repr[0], occ=goal_df_repr[1],
+	#									 picks=goal_df_repr[2], pickpct=goal_df_repr[3])
+	return render_template('index.html', goals=goal_df_repr)
 
 
 @app.route('/<name>')
